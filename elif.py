039899142ee79207b7aac34342997953cd86818f
NaveenPerla143 <naveenperla8@email.com>
2026-03-26 20:09:40 +0530

    Initial commit

---
+++ elif.py
@@ -0,0 +1,8 @@
+score=70
+
+if (score>=90):
+    print("Grade a")
+elif(score>=80):
+    print("Grade b")
+elif(score>=70):
+    print("Grade c")
--- reverse.py
+++ reverse.py
@@ -1,6 +1,7 @@
-txt="nawin yadav"
-print(txt[::-1])
+n=4
+f=1
 
+for i in range(1,n+1):
+    f*=i
 
-
-d,tjgnflikgjn
+print(f)
